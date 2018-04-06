e7a7a206eb328ed6cf3ff11217af5df0d562dfcc
wangyu19810820 <gadfly0036_cn@sina.com>
2018-04-06 18:14:39 +0800

    aa

---
+++ 入门/lesson02/type.js
@@ -0,0 +1,10 @@
+var a = "Hello world!";
+var b = 2;
+var c = true;
+var d = null;
+var e = undefined;
+function fun() {
+    console.log(1);
+}
+var f = 1;
+f = "abc";
---
+++ 入门/lesson02/type.ts
@@ -0,0 +1,12 @@
+var a:string = "Hello world!";
+var b:number = 2;
+var c:boolean = true;
+var d:null = null;
+var e:undefined = undefined;
+
+function fun():void {
+    console.log(1);
+}
+
+var f:any = 1;
+f = "abc";
